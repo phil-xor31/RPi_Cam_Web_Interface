186c7b9b6f7b55bc19f4d567dfb2aadbf76f6818
phil-xor31 <phil-xor31@users.noreply.github.com>
2014-07-19 23:51:31 -0700

    more jquery conversion

--- www/main.js
+++ www/main.js
@@ -1,14 +1,35 @@
 $(function() {
 
-  console.log("main.js loaded");
+  init();
 
   // event handlers for all the buttons and selectors on index.html
+
   $('#select_preset_res').on('change', function() {
-  	set_preset(this.value);
+    $("#video_width").val(this.value.substr(0, 4));
+    $("#video_height").val(this.value.substr(5, 4));
+    $("#video_fps").val(this.value.substr(10, 2));
+    $("#MP4Box_fps").val(this.value.substr(13, 2));
+    $("#image_width").val(this.value.substr(16, 4));
+    $("#image_height").val(this.value.substr(21, 4));
+    send_cmd("px " + this.value);
   });
   
   $('#set_res').on('click', function() {
-  	set_res();
+    while($("#video_width").val().length < 4) $("#video_width").val("0" + $("#video_width").val());
+    while($("#video_height").val().length < 4) $("#video_height").val("0" + $("#video_height").val());
+    while($("#video_fps").val().length < 2) $("#video_fps").val("0" + $("#video_fps").val());
+    while($("#MP4Box_fps").val().length < 2) $("#MP4Box_fps").val("0" + $("#MP4Box_fps").val());
+    while($("#image_width").val().length < 4) $("#image_width").val("0" + $("#image_width").val());
+    while($("#image_height").val().length < 4) $("#image_height").val("0" + $("#image_height").val());
+    
+    send_cmd("px " + 
+      $("#video_width").val() + " " + 
+      $("#video_height").val() + " " + 
+      $("#video_fps").val() + " " + 
+      $("#MP4Box_fps").val() + " " + 
+      $("#image_width").val() + " " + 
+      $("#image_height").val());
+
   });
 
   $('#set_sharpness').on('click', function() {
@@ -60,7 +81,13 @@ $(function() {
   });
 
   $('#set_ce').on('click', function() {
-  	set_ce();
+    while($("#ce_u").val().length < 3) $("#ce_u").val("0" + $("#ce_u").val());
+    while($("#ce_v").val().length < 3) $("#ce_v").val("0" + $("#ce_v").val());
+  
+    send_cmd("ce " + 
+      $("#ce_en").val() + " " + 
+      $("#ce_u").val() + " " + 
+      $("#ce_v").val());
   });
 
   $('#set_rotation').on('change', function() {
@@ -72,7 +99,17 @@ $(function() {
   });
 
   $('#set_roi').on('click', function() {
-  	set_roi();
+    while($("#roi_x").val().length < 5) $("#roi_x").val("0" + $("#roi_x").val());
+    while($("#roi_y").val().length < 5) $("#roi_y").val("0" + $("#roi_y").val());
+    while($("#roi_w").val().length < 5) $("#roi_w").val("0" + $("#roi_w").val());
+    while($("#roi_h").val().length < 5) $("#roi_h").val("0" + $("#roi_h").val());
+    
+    send_cmd("ri " + 
+      $("#roi_x").val() + " " + 
+      $("#roi_y").val() + " " + 
+      $("#roi_w").val() + " " + 
+      $("#roi_h").val());
+
   });
 
   $('#set_shutter').on('click', function() {
